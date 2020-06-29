192d7c00a900eef3d4d9bfff688b66427c0f6ae0
thayes46 <thayes46@gatech.edu>
2020-06-29 12:30:49 -0400

    add arduino file

---
+++ arduinoKBM/arduinoKBM.ino
@@ -0,0 +1,12 @@
+/*
+ * Arduino KBM for corys-shitty-robot
+ */
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
