70c1a99fcb81967650146ea0bd494912355f635b
thayes46 <thayes46@gatech.edu>
2020-06-29 14:15:02 -0400

    add basic I2C and KBM setup

--- arduinoKBM/arduinoKBM.ino
+++ arduinoKBM/arduinoKBM.ino
@@ -1,12 +1,26 @@
 /*
  * Arduino KBM for corys-shitty-robot
  */
-void setup() {
-  // put your setup code here, to run once:
+#include <Keyboard.h>
+#include <Mouse.h>
+#include <Wire.h>
+#define SLAVE_ADDRESS 0x04
 
+void setup() {
+  //Communication set up to prepare inputs and outputs
+  Wire.begin(SLAVE_ADDRESS);
+  Keyboard.begin();
+  Mouse.begin();
+  Wire.onReceive(receiveEvent);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop() { 
+  delay(100); //NOTE: remove/decrease this if notable lag, increase if overheating
+}
 
+/**
+ * Handler for events received on I2C for events
+ */
+void receiveEvent(int numEvents) {
+  
 }
