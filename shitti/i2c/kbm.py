70af31177071f702f0d2b8c0edd5c19562111aab
thayes46 <thayes46@gatech.edu>
2020-07-01 20:21:18 -0400

    Pi i2c backbone function. Filtering if it's a character or number for mouse values is a bit cursed, may be worth restructuring how we receive mouse movement calls on the arduino

--- shitti/i2c/kbm.py
+++ shitti/i2c/kbm.py
@@ -1,7 +1,19 @@
 import smbus
-import time
 
 bus = smbus.SMBus(1)
 
-#address for KBM is 0x04
+# address for KBM is 0x04
+
 addressKBM = 0x04
+
+
+def sendchar(value):
+    try:
+        if value[2] == '':
+            bus.write_byte(addressKBM, value)
+            return -1
+    except TypeError:   # catch when sending mouse values
+        if 127 >= value >= -128:
+            bus.write_byte(addressKBM, value)
+            return -1
+    return 0
