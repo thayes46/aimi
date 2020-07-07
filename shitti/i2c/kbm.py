9da0b071d4122cf09a778f9725172317fd55d26c
thayes46 <thayes46@gatech.edu>
2020-07-06 20:19:10 -0400

    Readability improvements + venv upate

--- shitti/i2c/kbm.py
+++ shitti/i2c/kbm.py
@@ -7,13 +7,15 @@ bus = smbus.SMBus(1)
 addressKBM = 0x04
 
 
+# function to send a byte over the i2c bus
+# returns 1 if success, 0 if failed
 def sendchar(value):
     try:
-        if value[2] == '':
+        if value[2] == '':  # ensure there is only 1 character
             bus.write_byte(addressKBM, value)
-            return -1
-    except TypeError:   # catch when sending mouse values
+            return 1
+    except TypeError:  # catch when sending mouse values
         if 127 >= value >= -128:
             bus.write_byte(addressKBM, value)
-            return -1
+            return 1
     return 0
