b59b154cc9325a2895f4c06d63f719e0ee6555fe
thayes46 <thayes46@gatech.edu>
2020-07-08 19:16:36 -0400

    SpAcEs aRe ImpoRTanT tO StYLe

--- shitti/processing/findCircle.py
+++ shitti/processing/findCircle.py
@@ -1,6 +1,7 @@
 import cv2 as cv
 import numpy as np
 
+
 # define function Find Circle
 # inputs: video source, minimum distance between circles, minimum circle radius,
 # and maximum circle radius
@@ -10,16 +11,15 @@ def findcircle(source, mindist, minrad, maxrad):
     # Capture Card
     cap = cv.VideoCapture(source)
 
-
     while 1:
         # read frame
-        _,frame = cap.read()
+        _, frame = cap.read()
 
         # find size of frame
         size = frame.shape
 
         # convert frame to grayscale
-        bwframe = cv.cvtColor(frame,cv.COLOR_BGR2GRAY)
+        bwframe = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
 
         # find circles using Hough Transformation. min and max radius will need
         # to be set when we have good values
@@ -32,23 +32,23 @@ def findcircle(source, mindist, minrad, maxrad):
 
         # iterate through list of circles. Make green circle around
         # circumference and small red dot at center
-        for i in circles[0,:]:
+        for i in circles[0, :]:
             # make sure that circle is inside the frame
             # can probably remove for Osu! but need for webcam
             if i[0] in range(size[0]) and i[1] in range(size[1]):
-                cv.circle(frame,(i[0],i[1]),i[2],(0,0,255),2)
-                cv.circle(frame,(i[0],i[1]),2,(0,255,255),3)
+                cv.circle(frame, (i[0], i[1]), i[2], (0, 0, 255), 2)
+                cv.circle(frame, (i[0], i[1]), 2, (0, 255, 255), 3)
 
                 # this should be getting the bgr values at the center of
                 # each circle
-                b = frame.item(int(i[0]),int(i[1]),0)
-                g = frame.item(int(i[0]),int(i[1]),1)
-                r = frame.item(int(i[0]),int(i[1]),2)
+                b = frame.item(int(i[0]), int(i[1]), 0)
+                g = frame.item(int(i[0]), int(i[1]), 1)
+                r = frame.item(int(i[0]), int(i[1]), 2)
 
                 # return x-center, y-center, radius, r value, g value, and
                 # b value and continue running function
                 # i[0] = x, i[1] = y, i[2] = radius
-                yield [i,r,g,b]
+                yield [i, r, g, b]
 
                 """# print centerpoint, radius, and color to console
                 print('Center: (%d,%d)' % (i[0],i[1]))
@@ -56,17 +56,18 @@ def findcircle(source, mindist, minrad, maxrad):
                 print('Color (RGB): (%d,%d,%d)' % (r,g,b))"""
 
         # show each frame and break loop when esc key pressed
-        cv.imshow('Feed',frame)
-        k=cv.waitKey(5) & 0xFF
-        if k==27:
+        cv.imshow('Feed', frame)
+        k = cv.waitKey(5) & 0xFF
+        if k == 27:
             break
 
     # turn off camera and close window
     cap.release()
     cv.destroyAllWindows()
 
+
 # function call
-ret = findcircle(0,500,0,50)
+ret = findcircle(0, 500, 0, 50)
 
 # prints ind for each iteration of the function
 # ind[0][0] = x, ind[0][1] = y, ind[0][2] = radius, ind[1] = r, ind[2] = g,
