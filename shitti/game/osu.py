dcfe96bd0a91e3f48d83d8d11448a804c71b7fc0
thayes46 <thayes46@gatech.edu>
2020-07-10 23:45:07 -0400

    yeet yeet motherfucker bitch

--- shitti/game/osu.py
+++ shitti/game/osu.py
@@ -1,8 +1,18 @@
 # script for the basics of playing osu
-from ..processing.circles import *
+from ..processing import circles
+import mss
+import numpy as np
+import cv2 as cv
 
 
 def run():
-    ret = findcircle("images/Circles.mp4", 50, 0, 100)
+    monitor = {'top': 0, 'left': 0, 'width': 600, 'height': 600}
+    sct = mss.mss()
+    image = sct.grab(monitor)
+    image = np.array(image)
+    image = np.flip(image[:, :, :3], 2)
+    image = cv.cvtColor(image, cv.COLOR_BGR2RGB)
+    print('before call')
+    ret = circles.houghcircle(image)
     for ind in ret:
         print(ind)
