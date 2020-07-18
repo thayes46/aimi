305c313c40d31d257a37983fbae907115af123a1
thayes46 <thayes46@gatech.edu>
2020-07-17 20:47:55 -0400

    [broken] why are keyboard listeners so complicated

--- shitti/game/osu.py
+++ shitti/game/osu.py
@@ -1,16 +1,39 @@
 from ..frameprocessing import detection
 from ..mouse import targeting
 from ..frameprocessing.circleprocessing import drawcircles
+from pynput import keyboard
 import cv2
+
+break_program = False
+monitor = {'top': 0, 'left': 550, 'width': 500, 'height': 500}
 
 
 def run():
+    global monitor
     running = True
-    # define area of screen to watch for circles
-    monitor = {'top': 0, 'left': 550, 'width': 500, 'height': 500}
     # Variable to control whether a window is shown or not
     showcircles = True
-    # do the vision thing
+
+    print("You are about to let this program take control of your mouse.\n"
+          "It will click in this region:")
+    print(monitor)
+    print("If everything is prepared type \"ok\". "
+          "Type anything else to quit")
+    typedinput = input()
+    if typedinput == "ok":
+        print("Ok. Starting. Press ESCAPE at any time to stop")
+    else:
+        print("Exiting")
+        return 0
+    global break_program
+    with keyboard.Listener(
+            on_press=on_press,
+            on_release=on_release) as listener:
+        listener.join()
+        while not break_program:
+            print("waiting")
+        return 0
+
     while running:
         detectionresults = detection.detectcircles(monitor)
         circles = detectionresults[0]
@@ -31,3 +54,18 @@ def run():
         if escapekey == 27:
             cv2.destroyAllWindows()
             running = False
+
+
+# Can't quite tell how to abstract this to an API call yet.
+# TODO: Abstract the below
+
+def on_press(key):
+    print('{0} pressed'.format(
+        key))
+
+
+def on_release(key):
+    global break_program
+    if key == keyboard.Key.esc:
+        break_program = True
+        return False
--- shitti/mouse/targeting.py
+++ shitti/mouse/targeting.py
@@ -6,7 +6,7 @@ import pyautogui as mouse
 
 
 def clicktarget(targetx, targety):
-    if (not mouse.onScreen(targetx, targety)):
+    if not mouse.onScreen(targetx, targety):
         print("target out of bounds")
         return 0
     mouse.moveTo(targetx, targety, durationinseconds=0.05)
