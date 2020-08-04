a39fc6864fe1b82fc10ac27632de7e25004a2967
csstine99 <csstine99@gmail.com>
2020-08-04 11:34:04 -0400

    Color Filtering
    Added some code for filtering red based on state of init paramater in detect_circles function.

--- aimi/processing/detection.py
+++ aimi/processing/detection.py
@@ -2,10 +2,10 @@ from .circleprocessing import hough_circle
 import time
 import mss
 from numpy import flip, array
-from cv2 import cvtColor, COLOR_RGB2BGR
+import cv2 as cv
 
 
-def detect_circles(monitor):
+def detect_circles(monitor, init = False):
     screen = mss.mss()
     # get time for framerate math
     last_time = time.time()
@@ -22,7 +22,24 @@ def detect_circles(monitor):
 
     # flipping shit bc dimensions are weird
     progress_frame = flip(progress_frame[:, :, :3], 2)
-    progress_frame = cvtColor(progress_frame, COLOR_RGB2BGR)
+    progress_frame = cv.cvtColor(progress_frame, cv.COLOR_RGB2HSV)
+
+    # limits of "red". Pure red is 0,255,255
+    lower_red = array([0,150,150])
+    upper_red = array([10,255,255])
+
+    # mask creation and application
+    mask = cv.inRange(progress_frame, lower_red, upper_red)
+
+    # if init is False, invert mask
+    if init == False:
+        mask = cv.bitwise_not(mask,mask)
+
+    # filter frame, either only show red or show everything but red    
+    progress_frame = cv.bitwise_and(progress_frame, progress_frame, mask=mask)
+
+    # change colorspace for use in hough_circle
+    progress_frame = cv.cvtColor(progress_Frame, cv.COLOR_HSV2RGB)
 
     try:
         # get an array of all the circles in the frame
