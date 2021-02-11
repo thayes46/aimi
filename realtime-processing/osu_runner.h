11af4cda4a63984d58d11817f19cc36515ff1479
thayes46 <todd.hayeshayes@gmail.com>
2021-02-10 21:02:53 -0500

    this is a little trick that will help us later

--- realtime-processing/osu_runner.h
+++ realtime-processing/osu_runner.h
@@ -18,7 +18,9 @@ std::mutex FRAME_MUTEX;
 void readStream();
 void selectFrame();
 /**
- * Use openCV to
+ * Use openCV to find current position as well as the targeted position
+ * in osu current is cursor in valorant current is enemy head/target
+ * in osu target is center of circle, in valorant, target is crosshair
  * @return 0 if successful, nonzero otherwise
  */
 int findTargets();
